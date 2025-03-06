624cab01cbbe4da680f8c00f2a0591fe9350699b
vaishnavw45 <vaishnavc2445@gmail.com>
2025-03-06 20:30:06 +0530

    commit

---
+++ src/Firebase/FirebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+const FirebaseApp = initializeApp(firebaseConfig);
+const FirebaseAuth = getAuth(FirebaseApp);
+const FirebaseFirestore = getFirestore(FirebaseApp);
+const FirebaseStorage = getStorage(FirebaseApp);
+
+const db = FirebaseFirestore;
+
+export { FirebaseApp, FirebaseAuth, FirebaseFirestore, FirebaseStorage, db };
